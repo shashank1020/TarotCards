a039530459ba8487757064e1e2612c11e30ff472
Shashank Maurya <shashankmaurya1020@gmail.com>
2025-03-12 17:43:59 +0530

    add fonts enum

---
+++ src/font.ts
@@ -0,0 +1,5 @@
+export const FONTS = {
+  ZillaSlabHighlight_Regular: 'ZillaSlabHighlight-Regular',
+  ZillaSlabHighlight_Bold: 'ZillaSlabHighlight-Bold',
+  MajorMonoDisplay_Regular: 'MajorMonoDisplay-Regular',
+};
